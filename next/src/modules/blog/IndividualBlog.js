4ced7efd08e460454c9880bbf7eb000b2c04d121
Amy Dutton <amy@ahhacreative.com>
2020-12-27 17:57:29 -0600

    🐛 Fixed date formatting
    - Was showing undefined undefined, undefined if no date was provided.

--- next/src/modules/blog/IndividualBlog.js
+++ next/src/modules/blog/IndividualBlog.js
@@ -10,6 +10,17 @@ import { Comments } from "modules/shared/components/Comments";
 
 const IndividualBlog = (props) => {
   const { content, hero, postDetails, title, titleWithinHero, video } = props;
+
+  const ShowPostDate = () => {
+    if (postDetails.dateUpdated) {
+      return (<h3 className="uppercase text-baliHai font-condensed tracking-wider text-2xl text-center mb-12">Last Updated: {prettyDate(postDetails.dateUpdated)}</h3>)
+    }
+    else if (postDetails.datePublished) {
+      return (<h3 className="uppercase text-baliHai font-condensed tracking-wider text-2xl text-center mb-12">Published: {prettyDate(postDetails.datePublished)}</h3>)
+    }
+    return '';
+  }
+
   return (
     <>
       <div className={`content-grid move-content-up ${titleWithinHero ? '' : 'no-title'}`}>
@@ -24,11 +35,7 @@ const IndividualBlog = (props) => {
           <div className="px-8 pt-8">
             <h1 className="font-condensed text-8xl uppercase text-center leading-stacked mb-8">{title}</h1>
             <hr className="border-t-4 border-black mb-8 max-w-md mx-auto" />
-            {postDetails.dateUpdated ?
-              (<h3 className="uppercase text-baliHai font-condensed tracking-wider text-2xl text-center mb-12">Last Updated: {prettyDate(postDetails.dateUpdated)}</h3>)
-              :
-              (<h3 className="uppercase text-baliHai font-condensed tracking-wider text-2xl text-center mb-12">Published: {prettyDate(postDetails.datePublished)}</h3>)
-            }
+            {ShowPostDate}
           </div>
         </div>
       </div>
