95654db560054f7b8ca0f9a19b9e67546c56ffe3
Amy Dutton <amy@ahhacreative.com>
2021-01-02 12:20:09 -0500

    ✨Added Fathom Analytics

--- next/src/pages/_app.js
+++ next/src/pages/_app.js
@@ -1,6 +1,29 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('YOUR_FATHOM_TRACKING_CODE', {
+      includedDomains: ['YOUR_SITE_URL'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />;
 }
 
