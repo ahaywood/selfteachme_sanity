1a1fdaf3dc2f8ebb4c532473aac27f2600c6ac41
Amy Dutton <amy@ahhacreative.com>
2021-01-02 12:23:24 -0500

    ✨ Added Twitter OG Meta Data to backend

--- sanity/schemas/postMeta.js
+++ sanity/schemas/postMeta.js
@@ -8,6 +8,10 @@ export default {
     { name: "ogTitle", title: "og Title", type: "string" },
     { name: "ogDescription", title: "og Description", type: "text" },
     { name: "ogImage", title: "og Image", type: "image" },
+    { name: "twitterTitle", title: "Twitter Title", type: "string" },
+    { name: "twitterDescription", title: "Twitter Description", type: "text" },
+    { name: "twitterImage", title: "Twitter Image", type: "image" },
+    { name: "twitterImageAlt", title: "Twitter Image Alt", type: "string" }
   ],
   options: {
     collapsible: true,
