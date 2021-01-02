f53931dddaaebf9fb3c258f060427b19c6abdb2a
Amy Dutton <amy@ahhacreative.com>
2021-01-02 12:22:44 -0500

    ✨Updated box display on frontend

--- next/src/modules/shared/blockContent/Box.js
+++ next/src/modules/shared/blockContent/Box.js
@@ -1,9 +1,16 @@
 const BlockContent = require("@sanity/block-content-to-react");
 import { serializers } from "modules/shared/blockContent/Serializers";
 
-const Box = (props) => (
-  <div className="col-start-4 mb-10 col-span-6 bg-almostWhite py-10 px-20">
-    <BlockContent blocks={props.node.content} serializers={serializers} />
-  </div>
-);
+const Box = (props) => {
+  const { content, emoji, heading } = props.node;
+  return (
+    <div className="col-start-4 mb-10 col-span-6 bg-almostWhite pt-10 pb-4 px-12 relative">
+      {emoji && <div className="text-5xl absolute top-8 -left-5">{emoji}</div>}
+      <div>
+        {heading && <h3 className="font-condensed uppercase text-baliHai text-5xl">{heading}</h3>}
+        <BlockContent blocks={content} serializers={serializers} />
+      </div>
+    </div>
+  )
+};
 export { Box };
