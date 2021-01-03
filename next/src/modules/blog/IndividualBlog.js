e240f58acfb4ad0ffb21e3dbf4e6ae7291d1b186
Amy Dutton <amy@ahhacreative.com>
2021-01-03 00:05:20 -0600

    🐛 Undid the work I just committed
    - Prevented Comments from loading properly

--- next/src/modules/blog/IndividualBlog.js
+++ next/src/modules/blog/IndividualBlog.js
@@ -51,12 +51,10 @@ const IndividualBlog = (props) => {
       </div>
 
       {/* pagination */}
-      { blogPagination || relatedPosts && (
-        <section className="grid grid-cols-12 bg-almostWhite pb-12 md:py-12 mt-8">
-          {blogPagination && <NextPreviousPosts blogPagination={blogPagination} />}
-          {relatedPosts && <RelatedPosts relatedPosts={relatedPosts} />}
-        </section>
-      )}
+      <section className="grid grid-cols-12 bg-almostWhite pb-12 md:py-12 mt-8">
+        {blogPagination && <NextPreviousPosts blogPagination={blogPagination} />}
+        {relatedPosts && <RelatedPosts relatedPosts={relatedPosts} />}
+      </section>
 
       <div className="mt-8">{/* form was not displaying correctly without the wrapping div 🤷🏻‍♀️ */}
         <EmailNewsletter />
