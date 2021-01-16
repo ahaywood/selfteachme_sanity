ab69c77b56b4f1905d9fc42fa07f9afc927d9600
Amy Dutton <amy@ahhacreative.com>
2021-01-16 11:38:40 -0600

    🐛Resolve issue with Commento

--- next/src/modules/shared/components/Comments.js
+++ next/src/modules/shared/components/Comments.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Hr } from "modules/shared/components/Hr";
 
 /** -------------------------------------------------
@@ -6,6 +7,16 @@ import { Hr } from "modules/shared/components/Hr";
 export const Comments = ({ className }) => {
   return (
     <div className={className}>
+      <Head>
+        {/* commento */}
+        <script
+          defer
+          src="https://cdn.commento.io/js/commento.js"
+          data-css-override="https://selfteach.me/css/commento.css"
+          data-auto-init="true"
+          type="text/javascript"
+        ></script>
+      </Head>
       <Hr />
       <h2 className="font-condensed text-brightPink uppercase text-6xl">Comments</h2>
       <div id="commento"></div>
