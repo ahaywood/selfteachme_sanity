c21b76188e77fb03079fcd4ace11b30ee662e107
Amy Dutton <amy@ahhacreative.com>
2021-01-16 11:49:11 -0600

    🚧 Moved comment to the document

--- next/src/modules/shared/components/Comments.js
+++ next/src/modules/shared/components/Comments.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Hr } from "modules/shared/components/Hr";
 
 /** -------------------------------------------------
@@ -9,15 +8,6 @@ export const Comments = ({ className }) => {
     <div className={className}>
       <Hr />
       <h2 className="font-condensed text-brightPink uppercase text-6xl">Comments</h2>
-      <Head>
-        {/* commento */}
-        <script
-          src="https://cdn.commento.io/js/commento.js"
-          data-css-override="https://selfteach.me/css/commento.css"
-          data-auto-init="true"
-          type="text/javascript"
-        ></script>
-      </Head>
       <div id="commento"></div>
     </div>
   );
