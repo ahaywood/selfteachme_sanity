14390597f41ad409de9c61e20605a3a43a05fd5a
Amy Dutton <amy@ahhacreative.com>
2021-01-16 12:41:41 -0600

    💄 Updated search display to be consistent with blog listing page

--- next/src/modules/search/SearchPage.js
+++ next/src/modules/search/SearchPage.js
@@ -20,15 +20,17 @@ const SearchPage = ({ content }) => {
     <div className="content-grid">
       <Hero title={displayResults(content.length)} subtitle={`Keyword: ${router.query.keywords}`} src="/img/bg__contact.jpg" className="hero" />
       <SearchBar className="col-span-3 mb-12" />
-      {/* content */}
-      {content?.map((post) => {
-        return (
-          <div className="stripe col-span-3" key={post._id}>
-            <FullWidthBlogCard buttonStyle="tertiary" content={post} />
-            <hr className="my-20 bg-horizontalPluses bg-no-repeat h-4 bg-center mx-auto max-w-6xl border-none" />
-          </div>
-        )
-      })}
+      <div className="full-width ping-pong pt-8">
+        {/* content */}
+        {content?.map((post) => {
+          return (
+            <div className="stripe col-span-3" key={post._id}>
+              <FullWidthBlogCard buttonStyle="tertiary" content={post} />
+              <hr className="my-20 bg-horizontalPluses bg-no-repeat h-4 bg-center mx-auto max-w-6xl border-none" />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
