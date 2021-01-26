15595db40437880e8e0d89b03bc1c3f427fd88b5
Amy Dutton <amy@ahhacreative.com>
2021-01-26 09:44:28 -0600

    🐛 Added a key to the logo grid

--- next/src/modules/about/components/LogoGrid.js
+++ next/src/modules/about/components/LogoGrid.js
@@ -12,8 +12,8 @@ const LogoGrid = () => {
 
   return (
     <div className="grid grid-cols-4 md:grid-cols-5 gap-5 sm:gap-16 items-center justify-items-center align-content-center px-4 lg:px-0">
-      {logos && logos.map(item => (
-        <div>
+      {logos && logos.map((item, index) => (
+        <div key={index}>
           <img src={item.logo.url} alt={item.name} className="max-w-1 max-h-11" />
         </div>
       ))}
