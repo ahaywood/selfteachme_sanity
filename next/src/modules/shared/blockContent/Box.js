8174bd717df26e451ff6ca993a49f59b1a80f3b3
Amy Dutton <amy@ahhacreative.com>
2021-01-26 09:44:41 -0600

    💄 Adjusted the styling on the box

--- next/src/modules/shared/blockContent/Box.js
+++ next/src/modules/shared/blockContent/Box.js
@@ -4,8 +4,8 @@ import { serializers } from "modules/shared/blockContent/Serializers";
 const Box = (props) => {
   const { content, emoji, heading } = props.node;
   return (
-    <div className="col-start-4 mb-10 col-span-6 bg-almostWhite pt-10 pb-4 px-12 relative">
-      {emoji && <div className="text-5xl absolute top-8 -left-5">{emoji}</div>}
+    <div className="col-start-4 mb-10 col-span-6 bg-almostWhite pt-10 pb-4 pl-20 pr-12 relative">
+      {emoji && <div className="text-7xl absolute top-4 left-1">{emoji}</div>}
       <div>
         {heading && <h3 className="font-condensed uppercase text-baliHai text-5xl">{heading}</h3>}
         <BlockContent blocks={content} serializers={serializers} />
