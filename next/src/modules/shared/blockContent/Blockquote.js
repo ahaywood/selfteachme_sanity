417c462d2990dfbe08c464d803f0524b2d25fea7
Amy Dutton <amy@ahhacreative.com>
2021-03-07 16:02:57 -0600

    ✨ Enhanced block quote

--- next/src/modules/shared/blockContent/Blockquote.js
+++ next/src/modules/shared/blockContent/Blockquote.js
@@ -1,6 +1,13 @@
-const Blockquote = (props) => (
-  <blockquote className="font-modern text-2xl text-center">
-    {props.children}
-  </blockquote>
-);
+
+
+const Blockquote = (props) => {
+  const { node: { type } } = props
+  console.log(type);
+  // console.log(props.node.type);
+  return (
+    <blockquote className="font-modern text-2xl text-center">
+
+    </blockquote>
+  )
+};
 export { Blockquote };
